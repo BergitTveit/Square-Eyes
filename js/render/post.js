90e7c885893c605af2824ffc4727d05e3d9ce6e1
BergitTveit <134147170+BergitTveit@users.noreply.github.com>
2023-11-08 16:52:50 +0100

    filmpage

--- js/render/post.js
+++ js/render/post.js
@@ -1,4 +1,8 @@
 export async function displayFilmDetails(film) {
+  const filmDetailsContainer = document.querySelector(".film-details");
+  const filmTitle = document.querySelector(".film-title");
+  const filmDetails = document.querySelector(".film-info");
+
   const filmElement = document.createElement("a");
   filmElement.href = "/film/?id=" + film.id;
   filmElement.class = "film-link";
@@ -8,7 +12,8 @@ export async function displayFilmDetails(film) {
   img.alt = film.title;
 
   filmElement.append(img);
+  filmDetailsContainer.prepend(filmElement);
 
-  const filmDetailsContainer = document.querySelector(".film-details");
-  filmDetailsContainer.append(filmElement);
+  filmTitle.textContent = film.title;
+  filmDetails.textContent = film.description;
 }
