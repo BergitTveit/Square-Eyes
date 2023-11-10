e33fb2203e78389992757003b80c8b29beb17aca
BergitTveit <134147170+BergitTveit@users.noreply.github.com>
2023-11-10 12:03:08 +0100

    working on CSS

--- js/bestseller.js
+++ js/bestseller.js
@@ -2,35 +2,25 @@ import { fetchFilmsSortedByReleased } from "./api/api.js";
 import { displayFilms } from "./render/list.js";
 
 export async function displayBestSellerFilm(film) {
-  const bestSellerContainer = document.querySelector(".best-seller");
-  //   const img = document.createElement("img");
-
-  //   img.src = film.image;
-  //   img.alt = film.title;
-  //   img.className = "film-img";
+  const bestSellerContainer = document.querySelector(".best-seller-cover");
 
   const filmElement = document.createElement("a");
   filmElement.href = "/film/?id=" + film.id;
-  filmElement.className = "film-link ";
+  filmElement.className = "film-link";
 
-  //   const backgroundDiv = document.createElement("div");
-  //   backgroundDiv.style.backgroundImage = `url(${film.image})`;
-  //   backgroundDiv.style.backgroundSize = "contain";
-  //   backgroundDiv.style.backgroundPosition = "center";
-  //   backgroundDiv.style.backgroundRepeat = "no-repeat";
-  //   backgroundDiv.style.height = "50vh"; // Set your fixed height
-  //   backgroundDiv.style.width = "100%";
+  const img = document.createElement("img");
+  img.src = film.image;
+  img.alt = film.title;
+  img.className = "best-seller";
 
-  //   filmElement.appendChild(img);
-
+  filmElement.appendChild(img);
   bestSellerContainer.append(filmElement);
 }
 
 async function main() {
   try {
     let sortedFilms = await fetchFilmsSortedByReleased(1);
-
-    displayFilms(sortedFilms, ".best-seller");
+    displayFilms(sortedFilms, ".best-seller-cover");
   } catch (error) {
     console.error("An error occurred in the main function", error);
   }
