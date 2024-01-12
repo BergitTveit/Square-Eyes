df569dc530f10e945663b7f296f67ba3576c3e0f
BergitTveit <134147170+BergitTveit@users.noreply.github.com>
2024-01-12 14:54:42 +0100

    Renders details page to show img, title, price

--- js/render/post.js
+++ js/render/post.js
@@ -2,12 +2,24 @@
 
 export function displayFilmDetails(film) {
   console.log("Displaying film details:", film);
+  console.log("Film object keys:", Object.keys(film));
   const filmDetailsContainer = document.querySelector(".film-details");
   const filmTitle = document.createElement("h2");
-
+  const filmImage = document.createElement("img");
+  const filmPrice = document.createElement("p");
   filmTitle.textContent = film.name;
 
-  // filmDetailsContainer.innerHTML = film.name;
+  filmImage.src = film.images[0]?.src;
+  filmImage.alt = film.name;
+  if (film.prices && film.prices.price) {
+    const majorUnitsPrice = parseFloat(film.prices.price) / 100;
+    filmPrice.textContent = `${majorUnitsPrice.toFixed(0)} ,-`;
+  } else {
+    filmPrice.textContent = "Price not available";
+  }
+  filmDetailsContainer.innerHTML = "";
 
   filmDetailsContainer.appendChild(filmTitle);
+  filmDetailsContainer.appendChild(filmImage);
+  filmDetailsContainer.appendChild(filmPrice);
 }
